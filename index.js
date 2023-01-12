1716580148eac2aca97c9b2126ca266b553e12b3
twj2 <brianteh007@gmail.com>
2023-01-12 09:15:37 +0800

    sample socket.io api

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const {Server} = require("socket.io");
+const { Socket } = require('dgram');
+const { send } = require('process');
+
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",//the domain where the app is
+        method:["GET","POST"],
+    },
+});
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+});
+
+io.on("connection", (socket)=>{
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("send_message",(data)=>{
+        console.log(data)
+        socket.broadcast.emit("receive_message",data)
+    });
+    
+});
+
+server.listen(3001,()=>{
+    console.log("Server running...");
+});
+
+
