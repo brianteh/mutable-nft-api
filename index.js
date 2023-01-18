a96b41a21513bd108e0700076cee090956159838
brianteh <58378750+brianteh@users.noreply.github.com>
2023-01-18 19:31:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:3000",//the domain where the app is
+        origin:"https://www.gamingtoad.ml/",//the domain where the app is
         method:["GET","POST"],
     },
 });
