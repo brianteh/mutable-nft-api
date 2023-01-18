3bb6f1f66af0ab8fb264fa0d7adf19d58a06fecc
twj2 <brianteh007@gmail.com>
2023-01-18 21:49:28 +0800

    updated package.json

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const http = require('http');
-const express = require('express');
-const cors = require('cors');
-const {Server} = require("socket.io");
+
+import http from 'http'
+import express from 'express'
+import cors from 'cors'
+import {Server} from 'socket.io'
 
 
 
@@ -16,17 +17,48 @@ const io = new Server(server,{
         method:["GET","POST"],
     },
 });
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-});
 
-io.on("connection", (socket)=>{
-    console.log(`User connected: ${socket.id}`);
+// app.get("/",(req,res)=>{
+//     res.send("Hello World")
+// });
 
+
+// initial configuration
+let users = {};
+const MAX_ROOMS = 10;
+const MAX_PLAYER = 100;
+const MAX_PLAYER_IN_ROOM = 2;
+let rooms = [];
+for(let i=0;i<MAX_ROOMS;i++){
+    rooms.push({room_id: i,num: 0})
+}
+
+
+io.of("/user").on("connection", (socket)=>{
+    
+    const socket_id = socket.id;
+
+    console.log(`User connected: ${socket_id}`);
+
+    // let available_room_ids = rooms.filter((room,index)=>{
+    //     return room.num<MAX_PLAYER_IN_ROOM
+    // })
+
+    // users[socket_id]=available_room_ids[0]
+
+    // socket.on('disconnect',()=>{
+        
+    //     delete users[socket_id]
+    //     rooms[users[soc]]
+    // })
     socket.on("send_message",(data)=>{
         console.log(data)
         socket.broadcast.emit("receive_message",data)
     });
+
+
+
+
     
 });
 
