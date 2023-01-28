b485f336e8d59ce382e5ffe29366ca4161111f0f
twj2 <brianteh007@gmail.com>
2023-01-28 23:12:36 +0800

    added game state

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import cors from 'cors'
 import {Server} from 'socket.io'
 import z from 'zod'
 
+const dotenv = await import('dotenv')
+dotenv.config()
+
 const app = express();
 app.use(cors());
 
@@ -27,7 +30,11 @@ for(let i=0;i<MAX_ROOMS;i++){
 }
 
 // game state
+let game_state={
+    all_player_pos:[],
+    all_player_attribute:[]
 
+}
 
 
 // miscellaneous functions
@@ -55,14 +62,24 @@ io.of("/user").on("connection", (socket)=>{
     const room_id = users[socket_id]
     socket.join(room_id)
 
+    game_state.all_player_pos.push({player_id:socket_id,x:400,y:360})
+    game_state.all_player_attribute.push({player_id:socket_id})// load from nft
+
+
     // Notice other players in the same room
     socket.to(room_id).emit("new_player", 
     {
         user_id:socket_id,
         user_property:{
-            walking_speed: 2,
-            jumping_power: 2
-        }// load from nft
+            pos:{
+                x:game_state.all_player_pos[game_state.all_player_pos.findIndex((e)=>{ return e.player_id == socket_id })].x,
+                y:game_state.all_player_pos[game_state.all_player_pos.findIndex((e)=>{ return e.player_id == socket_id })].y
+            },
+            attribute:{
+                // load from nft
+            }
+
+        }
     }
     )
 
@@ -78,8 +95,6 @@ io.of("/user").on("connection", (socket)=>{
         rooms[users[socket_id]].num-=1
         delete users[socket_id]
         
-        // console.log(users)
-        // console.log(rooms)
         console.log(`${socket_id} left room ${room_id}`)
     })
     
@@ -88,20 +103,29 @@ io.of("/user").on("connection", (socket)=>{
         let all_player = getKeysByValue(users,room_id)
         socket.emit("load_prev_player",{
             all_player:all_player,
-            user_property:{
-                walking_speed: 2,
-                jumping_power: 2
-            }// load from nft
+            all_player_property:{
+                all_player_pos: game_state.all_player_pos.filter((e)=>{return all_player.includes(e.player_id) }),
+                all_player_attribute: game_state.all_player_attribute.filter((e)=>{return all_player.includes(e.player_id) })
+            }
 
         })
-        console.log(room_id,all_player)
+        //console.log(room_id,all_player)
     })
 
     // Relaying movement data according to room
     socket.on("movement_self_player",(data)=>{
+
         socket.to(room_id).emit("movement_all_player",{user_id:data.user_id,user_x:data.user_x,user_y:data.user_y})
+        
+        let _index = game_state.all_player_pos.findIndex((e)=>{return e.player_id == data.user_id })
+        game_state.all_player_pos[_index].x = data.user_x
+        game_state.all_player_pos[_index].y = data.user_y
+        //game_state.all_player_pos.push({player_id:data.user_id,x:data.user_x,y:data.user_y})
+
         //console.log({user_id:data.user_id,user_x:data.user_x,user_y:data.user_y})
     })
+
+    
 
 
 
